6ea474437043a09a3294ba35dfe1b581eb1b53c0
CarysEH04 <129853556+CarysEH04@users.noreply.github.com>
2023-04-04 14:03:01 +0100

    Update TriangularNumberTest.py

--- TriangularNumberTest.py
+++ TriangularNumberTest.py
@@ -1,3 +1,4 @@
+#Tests if the number is a prime number using recursion. If it is, it prints out which triangular number it is
 def TriangularNumberTest(x, count = 1):
     z = 1 + count
     if x < 0:
