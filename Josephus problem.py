3cddb930e8b95e9ccb1f72eafa993b5c9461f2fc
Carys <129853556+CarysEH04@users.noreply.github.com>
2024-04-24 19:27:07 +0100

    Update Josephus problem.py
    Added a new function to return the suffix for formatting purposes

--- Josephus problem.py
+++ Josephus problem.py
@@ -2,16 +2,8 @@
 def josephus_problem(n, k):
     circle = list(range(1, n+1))
     index = k-1
-    not_killed = _rec(circle, index)
-    not_killed = str(not_killed)
-    if not_killed[len(not_killed)-1] == '1':
-        suffix = 'st'
-    elif not_killed[len(not_killed)-1] == '2':
-        suffix = 'nd'
-    elif not_killed[len(not_killed)-1] == '3':
-        suffix = 'rd'
-    else:
-        suffix = 'th'
+    not_killed = str(_rec(circle, index))
+    suffix = returnSuffix(not_killed)
     return 'The ' + str(not_killed) + suffix + ' person will survive'
 
 def _rec(size, kill):
@@ -23,5 +15,16 @@ def _rec(size, kill):
         size2 = size[kill+1:]
         size = size2+size1
         return _rec(size, kill)
+
+def returnSuffix(number):
+    if not_killed[len(not_killed)-1] == '1':
+        suffix = 'st'
+    elif not_killed[len(not_killed)-1] == '2':
+        suffix = 'nd'
+    elif not_killed[len(not_killed)-1] == '3':
+        suffix = 'rd'
+    else:
+        suffix = 'th'
+    return suffix
 
 print(josephus_problem(11, 2))
